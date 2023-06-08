4c2bf54b901f9f934044b48ccf06b5106c5a42dc
Ukeme David Eseme <skywalkershadow9@gmail.com>
2023-06-08 20:46:54 +0000

    docker

--- centos-installer.sh
+++ centos-installer.sh
@@ -6,7 +6,16 @@ echo "----------------------installer for centos 8-------------------------"
 sudo yum install yum-utils epel-release -y
 
 echo "----------------------installer Jenkins-------------------------"
+
 sudo yum install java wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
 sudo rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum install jenkins -y
+
+echo "----------------------installer Docker-------------------------"
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin --allowerasing
+
+
+echo "----------------------installer Jenkins-------------------------"
