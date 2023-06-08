78ad0dd9ce543cdf0e88d0e8f5dc2b01ca5ee8a0
Ukeme David Eseme <skywalkershadow9@gmail.com>
2023-06-08 20:45:12 +0000

    jenkins

--- centos-installer.sh
+++ centos-installer.sh
@@ -2,3 +2,11 @@
 
 echo "----------------------installer for centos 8-------------------------"
 
+
+sudo yum install yum-utils epel-release -y
+
+echo "----------------------installer Jenkins-------------------------"
+sudo yum install java wget -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
+sudo rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins -y
